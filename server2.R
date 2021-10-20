3278a82b91b464e2545ac700b9bfd057d57bdca3
Lauren Shelby <shelbyl23@mail.wlu.edu>
2021-10-20 15:48:39 -0400

    took out code from Shiny widget

--- server2.R
+++ server2.R
@@ -9,7 +9,6 @@ function(input,output) {
   leafletOutput$worldMap <-
   
     wine_data <- read_xlsx ("Wine Location.xlsx")
-  ##wine_loc<- data.frame(wine_loc)
     wine_lat <- wine_data$Latitude
     wine_lon <- wine_data$Longitude
     wine_price <- wine_data$price
@@ -24,20 +23,7 @@ function(input,output) {
       addCircleMarkers(radius = ~points, label = ~winery)
 }
   
-  ##wine_loc$Latitude<- as.numeric(wine_loc$Latitude)
-  ##wine_loc$Longitude<- as.numeric(wine_loc$Longitude)
-  ##wine_loc= filter(wine_loc, Latitude != "NA")
-  
-  ##wine_loc<- mutate(wine_loc, cntnt= paste0('<strong>Name: </strong>', variety, 
-                                            ##'<br><strong> price: </strong>'
-  ##output$winemap <- renderLeaflet ({
-    ##leaflet(wine_loc) %>%
-      ##addCircles(lng = ~Longitude, lat = ~Latitude) %>%
-      ##addTiles() %>%
-      ##addCircleMarkers(data = wine_loc, lat = ~Latitude, lng = ~Longitude, 
-                       ##radius = 3, popup = ~as.character(cntnt), 
-                       ##color= ~pal(Category), 
-                       ##stroke = FALSE, fillOpacity = 0.8)})
+
     
 
   
