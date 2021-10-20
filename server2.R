c9e184dae13bcd7b2050598b57bbfc9986a1bc78
Lauren Shelby <shelbyl23@mail.wlu.edu>
2021-10-20 16:12:06 -0400

    notes on how to fix issues with loading the columns from the Wine Location excell sheet

--- server2.R
+++ server2.R
@@ -1,26 +1,32 @@
 library(shiny)
-library(dplyr)
 library(leaflet)
-library(DT)
+library(readxl)
 
 
 function(input,output) {
   
-  leafletOutput$worldMap <-
-  
-    wine_data <- read_xlsx ("Wine Location.xlsx")
-    wine_lat <- wine_data$Latitude
+  output$worldMap <- renderLeaflet({
+    
+    #pull excell columns for all columns of interest into wine_data using readxl::anchored
+    #might not need this variable if you can pull direct columns into individual vairables 
+    wine_data <- readxl::("Wine Location.xlsx")
+    #pull excell column for latitude, longitude, price, and points using readxl
+    #look into if readxl:: col can take the excell sheet as an input 
+    wine_lat <- 
     wine_lon <- wine_data$Longitude
     wine_price <- wine_data$price
     wine_rating <- wine_data$points
-  
+    
     wine_loc <- data.frame(wine_lat, wine_lon, wine_price, wine_rating, stringsAsFactors = FALSE)
-  
-  
+    
+    
     leaflet(data = wine_loc) %>%
       addTiles() %>% 
       addMarkers(~Longitude, ~Latitude) %>%
       addCircleMarkers(radius = ~points, label = ~winery)
+  })
+  
+   
 }
   
 
