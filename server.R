6641404b27238b9d2c263519d0aa9a1dee1e6c10
Lauren Shelby <shelbyl23@mail.wlu.edu>
2021-10-25 15:50:54 -0400

    first working map!

--- server.R
+++ server.R
@@ -4,42 +4,51 @@ library(ggplot2)
 
 #making a function that summarizes the data table
 function (input, output) {
+  dataset <- read.csv("Wine Location2.csv")
+  #View(dataset)
+  wine_loc <- data.frame(dataset, stringAsFactors = FALSE)
   
+  wine_loc$Latitude <- as.numeric(wine_loc$Latitude)
+  wine_loc$Longitude <- as.numeric(wine_loc$Longitude)
+  wine_loc$price <- as.numeric(wine_loc$price)
+  wine_loc$points <- as.numeric(wine_loc$points)
+  
+  output$wineMap <- renderLeaflet({
+    
+    leaflet(wine_loc) %>%
+      addTiles()%>%
+      addMarkers(~Longitude, ~Latitude) %>%
+      addCircleMarkers(radius = ~points, label = ~winery)
+  })
+  
+  observe({
+    btn <- input$newButton
+    
+  })
 }
 
 
-library(ggplot2)
-
-function (input, output) {
+#function (input, output) {
   
-  output$table <- DT:: renderDataTable(DT::datatable({ 
-    data <- wine_data
-    if (input$country != "ALL") {
-      data<- data[data$country == input$country,]
-    }
-    if (input$price != "ALL") {
-      data<- data[data$price == input$price,]
-    }
-    if (input$points != "ALL") {
-      data<- data[data$points == input$points,]
-    }
-    data
+  #output$table <- DT:: renderDataTable(DT::datatable({ 
+    #data <- wine_data
+    #if (input$country != "ALL") {
+      #data<- data[data$country == input$country,]
+    #}
+    #if (input$price != "ALL") {
+      #data<- data[data$price == input$price,]
+    #}
+    #if (input$points != "ALL") {
+      #data<- data[data$points == input$points,]
+    #}
+    #data
     
     
-    }))
+    #}))
   
   
   
   
-}
+#}
 
 
-  
-  
-  
-  
-  
-  
-  
-  
->>>>>>> 66c392f406a38e442e25557ebda56a72b9f6878b
--- ui.R
+++ ui.R
@@ -2,34 +2,43 @@
 library(shiny)
 library(leaflet)
 
-
-
-library(ggplot2)
 fluidPage(
-  titlePanel("Wine Data Table"), 
-  fluidRow(
-    column(2, 
-           selectInput("country", 
-                       "Country:", 
-                       c("All",
-                         unique(as.character(wine_data$country))))
-    ), 
-    column (6, 
-            selectInput("price", 
-                        "Price:", 
-                        c("ALL", 
-                          unique(as.character(wine_data$price))))
-            ), 
-    column (5, 
-            selectInput("points", 
-                        "Points:", 
-                        c("ALL", 
-                          unique(as.character(wine_data$points))))
-    )
-  ), 
-  DT:: dataTableOutput("table")
+  # map output 
+  leafletOutput("wineMap"),
+  
+  # line break 
+  br(), 
+  
+  # button 
+  actionButton("newButton", "New wine!")
+)
+
+#library(ggplot2)
+#fluidPage(
+  #titlePanel("Wine Data Table"), 
+  #fluidRow(
+    #column(2, 
+          # selectInput("country", 
+                      # "Country:", 
+                      #c("All",
+                        # unique(as.character(wine_data$country))))
+    #), 
+    #column (6, 
+           # selectInput("price", 
+                       # "Price:", 
+                       # c("ALL", 
+                         # unique(as.character(wine_data$price))))
+        #    ), 
+   # column (5, 
+           # selectInput("points", 
+                    #    "Points:", 
+                      #  c("ALL", 
+                    #      unique(as.character(wine_data$points))))
+#    )
+#  ), 
+#  DT:: dataTableOutput("table")
                                                 
-  )
+#  )
   
   
 
