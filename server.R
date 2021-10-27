8747a48dcc7ab1f197f37ed45be3c981770fbb38
Emma Grice <gricee22@mail.wlu.edu>
2021-10-27 14:50:19 -0400

    clustering of markers and title

--- server.R
+++ server.R
@@ -12,25 +12,73 @@ function (input, output) {
   wine_loc$Longitude <- as.numeric(wine_loc$Longitude)
   wine_loc$price <- as.numeric(wine_loc$price)
   wine_loc$points <- as.numeric(wine_loc$points)
+  wine_loc$Latitude<- jitter(wine_loc$Latitude, factor = 0.01)
+  wine_loc$Longitude<- jitter(wine_loc$Longitude, factor = 0.01)
+
+  
+ #wine_loc$pointsrange= cut(wine_loc$points, 
+                            # breaks = c( 96, 97, 98, 99, 100), right= FALSE,
+                            # labels= c("96-97", "98", "99", "100"))
+            
+
+                            
+                            
+  #add 5 colors, make legend 
+  
+ pal <- colorFactor(pal = c("green", "blue", "red", "yellow", "black"), domain = c("96", "97", "98", "99", "100"))
+ 
   
   output$wineMap <- renderLeaflet({
     
     leaflet(wine_loc) %>%
+      #addCircles(~Longitude, ~Latitude, fillopacity = 0.5) %>%
       addTiles()%>%
-      addMarkers(~Longitude, ~Latitude) %>%
-      addCircleMarkers(radius = ~points, label = ~winery)
-  })
-  
-  observe({
+      #addMarkers(~Longitude, ~Latitude, label = ~winery, popup = ~as.character(description)) %>%
+      #some lat and long values arent showing up 
+      addCircleMarkers(lng = ~Longitude, lat = ~Latitude, weight = 1, popup = ~winery, clusterOptions = markerClusterOptions())
+      
+                       #color = ~pal (points)) 
+      #addLegend(
+       # "bottomleft"
+        #values = ~points,
+        #pal = color, 
+        #title= "Wine by Rating"
+    #  )
+      #(wine_loc, position= "topright", pal=pal, values = wine_loc$points, opacity = 1, na.label = "Not Availible")
+    
+    })
+   
+    observe({
     btn <- input$newButton
     
   })
 }
+    
+#getColor<- function(wine_loc) {
+  #sapply(wine_loc$points, function(points) {
+  #if (points<= 100 ) {
+    #"green"
+ ##} else if (points <= 99){
+   # "orange"
+  #} else {
+   # "red"
+ # }})
+  
+
+   #  })
+  
+  
+  
+#}
 
 
 #function (input, output) {
   
-  #output$table <- DT:: renderDataTable(DT::datatable({ 
+  #output$data <- DT:: renderDataTable (datatable(
+   # wine_loc), filter= "top", 
+   # colnames= c("Country", "Description", "Points", "Price", "Province", 
+               # "Region 1", "Region 2", "Variety", "Winery", "Lat", "Long"))
+  
     #data <- wine_data
     #if (input$country != "ALL") {
       #data<- data[data$country == input$country,]
--- ui.R
+++ ui.R
@@ -2,16 +2,35 @@
 library(shiny)
 library(leaflet)
 
+
 fluidPage(
   # map output 
-  leafletOutput("wineMap"),
-  
+  titlePanel("The Locations of the Most Highly Rated Wines"),
+  mainPanel(leafletOutput("wineMap"))
+ # absolutePanel(top= 10, right = 10,
+                #selectInput(inputId= "country", 
+                            #label = "country", 
+                            #choices = "Argentina", "Australia", 
+                            #"Austria", "France", "Germany", 
+                           # "Hungary" ))
+)
+  #navbarPage("Locations of Highly Ranked Wines", id= "main", 
+            #tabPanel("Map", leafletOutput("wineMap", height= 1000)), 
+            # tabPanel("Data", DT::dataTableOutput("data")))
   # line break 
-  br(), 
+  #br(), 
   
   # button 
-  actionButton("newButton", "New wine!")
-)
+  #actionButton("newButton", "New wine!")
+  
+  #absolutePanel(top = 10, right = 10, 
+               # selectInput("country"))
+  #label= "Select a Country:", choices: list("US", "France"))
+
+      
+                 # 'live-search'=TRUE
+                #)
+
 
 #library(ggplot2)
 #fluidPage(
