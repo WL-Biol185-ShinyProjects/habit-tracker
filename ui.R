601a85eb621456f832c8bdf3e876bda4185810c9
Lauren Shelby <shelbyl23@mail.wlu.edu>
2021-11-15 15:24:58 -0500

    aesthetic changes to introduction page

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(ggplot2)
 #source("dataTable.R")
 Country_Wine_Production <- read.csv("Country Wine Production.csv")
 
-dashboardPage(skin = "red", 
+dashboardPage(skin = "purple", 
               dashboardHeader(title = "Wine Dashboard", titleWidth = 600), 
               dashboardSidebar(width = 240,
                 sidebarMenu(
@@ -30,49 +30,50 @@ dashboardPage(skin = "red",
                   tabItem(tabName = "intro", 
                           fluidRow(
                             titlePanel ("Introduction"),
-                            box(
-                              title = "Wine History & Facts", width = NULL, background = "red", 
-                              "Wine has been a part of society for thousands of years, and the 
+                            column(width = 6,
+                                   box(
+                                     title = "Wine History & Facts", width = NULL, background = "olive", 
+                                     "Wine has been a part of society for thousands of years, and the 
                               oldest winery in the world was dated back to 4100 BC in Ancient 
                               Armenia. In these ancient times, due to its resemblance to blood,
                               wine was used mainly in religious ceremonies. It is even mentioned 
                               in the Bible. As trade grew, wine was brought to the Greeks around 
                               800 BC who expanded it with the Roman Empire and the adoption of 
                               Christianity."), 
-                            box(
-                              title = "Types of Wines", width = NULL, background = "red", 
-                              "There are more than 10,000 varieties of grapes grown in the world, 
+                                   box(
+                                     title = "Types of Wines", width = NULL, background = "olive", 
+                                     "There are more than 10,000 varieties of grapes grown in the world, 
                               but there are about 17 types of wine made with combinations of these
                               grapes. Types of red wine include: Pinot noir, cabernet franc, cabernet
                               sauvignon, carmenere, merlot, malbec, petit verdot, grenache, tempranillo,
                               primitivo, sangiovese, syrah/shiraz, and zinfandel. Types of white wine include: 
                               Chardonnay, pinot grigio, sauvignon blanc, and riesling. These types of wine are 
-                              categorized by taste in 4 ways: fruit, body, tanin, and acidity."), 
-                            box(
-                              title = "About Grape Growing", width = NULL, background = "red", 
-                              "Some vineyards plant multiple types of grapes, while others stick 
+                              categorized by taste in 4 ways: fruit, body, tanin, and acidity.")
+                            ),
+                            column(
+                              width=6,
+                              box(
+                                title = "About Grape Growing", width = NULL, background = "olive", 
+                                "Some vineyards plant multiple types of grapes, while others stick 
                               to one. It takes several years after planting to get the first harvest 
                               of grapes, and the vines require a lot of care in the meantime. Grape 
                               vines need a lot of water, pruning, and protection from disease, insects,
                               and animals. Taking good care of the grapes and being patient during the
                               fermentation process results in better wines."), 
-                            box(
-                              title = "Grape Growing and Climate Change", width = NULL, background = "red", 
-                              "Since grapes are grown on every continent in the world, the wine industry is 
+                              box(
+                                title = "Grape Growing and Climate Change", width = NULL, background = "olive", 
+                                "Since grapes are grown on every continent in the world, the wine industry is 
                               very expansive. However, recent climate change may put grape growing in jeopardy.
                               Different types of grapes are grown optimally at different temperature ranges. 
                               While this allows for different areas to make different wine based on the climate, 
                               if wine growers do not change the types of grapes they grow the yield could decrease
                               up to 56% with a temperature change of 2 degrees Celcius. In this website, we want 
                               to share these risks while also offering an informative user experience about wines 
-                              in general."),
-                            br(), 
+                              in general.")
+                                  ),
                             div(img(src="GrapeVine.jpg"), style= "text-align: center;")
-                           
-                    
-                            
-                    
-              )), 
+                                  )
+                                ),
                   tabItem(tabName = "map", 
                           fluidRow(
                             p("Choose a country to see each of its most highly ranked wines and their information!"), 
@@ -111,7 +112,7 @@ dashboardPage(skin = "red",
                                       )
                                     )
                                   )
-                         ), 
+                         ) ,
               
                     tabItem(tabName = "growth", 
                             fluidRow(
@@ -148,7 +149,11 @@ dashboardPage(skin = "red",
                                 )
                           
                                 )
-                              )))
+                        )
+              )
+)
+                              
+
                             
                     
     
